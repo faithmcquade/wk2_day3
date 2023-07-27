14874575c85cbcbcfc0c473d28e895ad6b8dbde4
faithannmarie <faithannmarie@bunnies.lan>
2023-07-27 14:38:40 -0700

    homework

---
+++ my_module.py
@@ -0,0 +1,9 @@
+#calculate square footage of a house
+def calculate_square_footage(length, width):
+    area = length * width
+    return area
+
+#calculate circumference of a circle
+def calculate_circumference(radius):
+    circumference = 2 * 3.14159 * radius
+    return circumference
